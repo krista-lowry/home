cfce4665bdac7746b21caa01b0a6b3c25e842e55
Krista Lowry <kristahlowry@gmail.com>
2025-01-06 17:12:35 -0800

    fix url

--- scripts/pages.js
+++ scripts/pages.js
@@ -3,7 +3,7 @@ const build_path = './out';
 
 const args = process.argv.slice(2);
 const username = args[0];
-const repoURL = `https://github.com/${username}/${username}.github.io.git`;
+const repoURL = `https://krista-lowry.github.io/home/`;
 const branch = args[1];
 
 console.log(`Deploying to ${repoURL} branch ${branch}`);
