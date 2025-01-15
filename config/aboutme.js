f8859507abb5a860ac15a72ff19d493a3b538ada
Krista Lowry <kristahlowry@gmail.com>
2025-01-15 10:09:31 -0800

    add experience

--- config/aboutme.js
+++ config/aboutme.js
@@ -15,25 +15,45 @@ export const aboutme_content = {
 		<hr/>
 		<h1 className ="mt-5 mb-3">Education</h1>
 		<p className="lead">I am a lifelong and eager learner, continually pursuing new courses and certifications to expand my skillset.</p>
-		<h4 class="blockquote text-primary mt-5"> My Certificates & Degree</h4>
-		<div class="row card-body my-4 shadow-sm p-3 bg-body rounded  col-sm-6">
+		<div class="row">
+		<div class="col-sm-6">
+		<div class="card-body my-4 shadow-sm p-3 bg-body rounded">
 		<h6 class="card-title">Certificate of Data Analytics: Techniques for Decision Making, 2023, University of Washington</h6>	
-		<div class="mt-3">Studied exploratory data analysis techniques using RStudio and SQL. Conducted predictive analysis using machine learning models such as linear and logistic regression, KNN clustering.</div></div>
-		</div>
-		<div class="row card-body my-4 shadow-sm p-3 bg-body rounded col-sm-6">
-	
+		<p class="mt-3">Studied exploratory data analysis techniques using RStudio and SQL. Conducted predictive analysis using machine learning models such as linear and logistic regression, KNN clustering.</p>		
+		</div></div>
+		<div class="col-sm-6">
+		<div class="card-body my-4 shadow-sm p-3 bg-body rounded">
 		<h6 class="card-title">Psychology Architectures of Digital Behavior Change, 2016, Behavioral Design Academy</h6>
 		<div class="mt-3">Studied behavioral change theories and psychological design patterns as they relate to digital experiences. Translated these principles into wireframes and user journey maps</div></div>
-	
-		<div class="row card-body my-4 shadow-sm p-3 bg-body rounded col-sm-6">
+		</div>	
+		<div class="row">
+		<div class="col-sm-6">
+		<div class="card-body my-4 shadow-sm p-3 bg-body rounded">
 		<h6 class="card-title">Certificate in Digital Marketing Analytics, 2015, University of Washington</h6>
-		<div class="mt-3">Studied digital marketing strategies and techniques across channels such as paid acquisition, SEO, display, affiliate and social media marketing. Built marketing plans to measure ROI.</div></div>
-		</div>
-		<div class="row card-body my-4 shadow-sm p-3 bg-body rounded">
-		<div class= "col-sm-6">
-		<h6 class="card-title">Bachelor of Arts in Philosophy, Graduated in 2012, cum laude. GPA: 3.58 SUNY Purchase College</h6>
-		<div className="card-body"> Specialized in phenomenology and existentialism philosophies
-		</div>
+		<p class="mt-3">Studied digital marketing strategies and techniques across channels such as paid acquisition, SEO, display, affiliate and social media marketing. Built marketing plans to measure ROI.</p>
 		</div></div>
+		<div class="col-sm-6">
+		<div class="card-body my-4 shadow-sm p-3 bg-body rounded">
+		<h6 class="card-title">Bachelor of Arts in Philosophy, Graduated in 2012, cum laude. GPA: 3.58 SUNY Purchase College</h6>
+		<p className="mt-3"> Specialized in phenomenology and existentialism philosophies
+		</p>
+		</div></div>
+		<h1 className ="mt-5 mb-3">Experience</h1>
+		<div class="row">
+		<div class="col-sm-6">
+		<div class="card-body my-4 shadow-sm p-3 bg-body rounded">
+		<h6 class="card-title">Senior Optimization Analyst,<small> 2021-present</small></h6>
+		<p class="mt-3">Responsible for driving experimentation across publishing and search businesses, performing high-visibility data analysis for key business initiatives, and partnering closely with engineering and design teams to build and test new features.</p></div>
+		</div>
+		<div class="col-sm-6">
+		<div class="card-body my-4 shadow-sm p-3 bg-body rounded">
+		<h6 class="card-title">Marketing Data & BI Analyst, <small> 2017-2021</small></h6>	
+		<p class="mt-3">Served as analytics and optimization lead on the team that built and launched a new subsidiary company, while also leading analytics for the parent company. Focused on building out the web analytics reporting stack, supporting UX design decisions with user behavior data and research, and improving data literacy across the organization.</p></div>
+		</div>
+		<div class="col-sm-6">
+		<div class="card-body my-4 shadow-sm p-3 bg-body rounded">
+		<h6 class="card-title">Digital Marketing & Web Coordinator, <small> 2014-2017</small></h6>
+		<p class="mt-3">Promoted to position after 1.5 years as editorial/production assistant. Assumed ownership of web analytics and online content management. Was part of the core team that launched a major website redesign, serving as the analytics and CMS lead, working closely with engineering.</p></div>
+		</div>			
 		`,
 }
